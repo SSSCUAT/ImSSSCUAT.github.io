5949146c574a794fd5ae467db8f1d0200abb1aa4
SSSCUAT <reynosodiego264@gmail.com>
2024-08-08 18:00:30 -0700

    changed styling for page and hover button fucntion

--- ap.js
+++ ap.js
@@ -1,18 +1,16 @@
 var pics = [
-    "images.Diego/IMG_2005 (1).jpg", //0
-    "images.Diego/IMG_2266.jpg", //1
-    "images.Diego/IMG_1236.jpg", //2
-    "images.Diego/IMG_3926.jpg", //3
-    "images.Diego/IMG_3927.jpg", //4
-    "images.Diego/IMG_5029.jpg", //5
-    "images.Diego/IMG_5442.jpg" //6
+    "images.Diego/IMG_5442.jpg", //0
+    "images.Diego/IMG_1236.jpg", //1
+    "images.Diego/IMG_3926.jpg", //2
+    "images.Diego/IMG_3927.jpg", //3
+    "images.Diego/IMG_5029.jpg" //4
 ]
 var btn = document.querySelector("button")
 var img = document.querySelector("img");
 var counter = 1;
 
 btn.addEventListener("click", function(){
-    if(counter === 7 ){
+    if(counter === 5 ){
         counter = 0;
     }
     img.src = pics[counter]
